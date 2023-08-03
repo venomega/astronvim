949d178430fd826d347580b4dc032d953c129536
venomega <guilleps92@gmail.com>
2023-08-03 04:28:09 +0000

    fix type

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,9 +2,6 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-<<<<<<< HEAD
-=======
->>>>>>> 29479f76ec6aa22ab941a735805f2b434b4817fe
   { "jbyuki/instant.nvim",     event = "VimEnter" },
   { "jbyuki/instant.nvim", event = "VimEnter" },
   {'rockerBOO/boo-colorscheme-nvim', event = "VimEnter"}
